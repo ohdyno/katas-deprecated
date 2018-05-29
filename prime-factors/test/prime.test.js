c252b47b07cc5d445ec85df935a742b92beabccc
Xing Zhou <xzhou@pivotal.io>
2018-05-29 10:51:30 -0400

    Handle primes of 2

--- prime-factors/test/prime.test.js
+++ prime-factors/test/prime.test.js
@@ -1,7 +1,13 @@
 function primes(number) {
-    return []
+    if (number === 1)
+        return [];
+    return [1, 2]
 }
 
-test('1 has no primes', () => {
+test('1 has no primes', function () {
     expect(primes(1)).toEqual([]);
+});
+
+test('2 has primes of 1 and 2', function () {
+    expect(primes(2)).toEqual([1, 2])
 });
