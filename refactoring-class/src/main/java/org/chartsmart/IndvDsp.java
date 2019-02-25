b46eaf2f4544fb1c1c0c4756fa852b573e624c47
xing <xing@integral.io>
2019-02-25 11:03:15 -0500

    Removed unreachable code, comments and variables

--- refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
+++ refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
@@ -61,12 +61,10 @@ public class IndvDsp extends JPanel {
     }
 
     private String tmStmp() {
-        // TODO Auto-generated method stub
         return new Date().toString();
     }
 
     private void DrawChart(Graphics g) {
-        // Render chart background
         if (ct == 406) {
             if (jjD.equals("rpfll")) {
                 Color bgc = Color.RED;
@@ -117,17 +115,9 @@ public class IndvDsp extends JPanel {
         if (ct == 406) {
             if (jjD.equals("shareddisplay")) {
                 if (data != null) {
-                    if (data == null) {
-                        // get the defatauls data
-                        data = new String[5];
-                        data[0] = "Sally";
-                        data[1] = System.getProperty("osname");
-                        data[2] = tmStmp();
-                    }
                     font = new Font("Arial Black", Font.BOLD, 25);
                     g.setColor(Color.CYAN);
                     int bottomY = 300;
-                    int startX = 100;
                     g.fillRect(100, bottomY - 100, 40, 100);
                     g.fillRect(140, bottomY - 200, 40, 200);
                     g.fillRect(180, bottomY - 150, 40, 150);
