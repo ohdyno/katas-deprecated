2880853aab15deff01194bd05be3db86a289b17a
xing <xing@integral.io>
2019-02-25 14:05:52 -0500

    renamed data to barChartData and g to graphics

--- refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
+++ refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
@@ -45,46 +45,46 @@ public class IndvDsp extends JPanel {
         DrawChart(g);
     }
 
-    private void DrawChart(Graphics g) {
+    private void DrawChart(Graphics graphics) {
         if (chartModeNumber == 406) {
             if (chartMode.equals("rpfll")) {
                 Color backgroundColor = Color.RED;
-                g.setColor(backgroundColor);
-                g.fillRect(100, 90, getWidth() - 200, 420);
+                graphics.setColor(backgroundColor);
+                graphics.fillRect(100, 90, getWidth() - 200, 420);
             } else {
-                g.setColor(Color.BLACK);
-                g.fillRect(95, 95, 210, 210);
+                graphics.setColor(Color.BLACK);
+                graphics.fillRect(95, 95, 210, 210);
             }
         } else {
             if (chartMode.equals("rpfll")) {
                 Color backgroundColor;
                 backgroundColor = Color.BLUE;
-                g.setColor(backgroundColor);
-                g.fillOval(100, 100, 450, getHeight() - 150);
+                graphics.setColor(backgroundColor);
+                graphics.fillOval(100, 100, 450, getHeight() - 150);
             } else {
-                g.setColor(Color.BLUE);
+                graphics.setColor(Color.BLUE);
                 double isq = 405;
                 float padding = 90;
                 int sc = (int) (isq - padding * 2);
-                g.fillOval(100, 100, sc, sc);
+                graphics.fillOval(100, 100, sc, sc);
             }
         }
-        String[] data = null;
-        List<String> specialData = new ArrayList<>();
+        String[] barChartData = null;
+        List<String> largePieChartData = new ArrayList<>();
         String[] smallPieChartData = new String[0];
         if (chartModeNumber == 406) {
             if (chartMode.equals("rpfll")) {
-                data = new String[1];
-                data[0] = "Bar Chart";
+                barChartData = new String[1];
+                barChartData[0] = "Bar Chart";
             } else {
-                data = new String[2];
+                barChartData = new String[2];
                 int i = 0;
-                data[i++] = "Bar Chart";
-                data[i] = "Small";
+                barChartData[i++] = "Bar Chart";
+                barChartData[i] = "Small";
             }
         } else {
             if (chartMode.equals("rpfll")) {
-                specialData.add("Pie Chart");
+                largePieChartData.add("Pie Chart");
             } else {
                 smallPieChartData = new String[2];
                 smallPieChartData[1] = "Small";
@@ -94,48 +94,48 @@ public class IndvDsp extends JPanel {
         Font font;
         if (chartModeNumber == 406) {
             if (chartMode.equals("shareddisplay")) {
-                if (data != null) {
+                if (barChartData != null) {
                     font = new Font("Arial Black", Font.BOLD, 25);
-                    g.setColor(Color.CYAN);
+                    graphics.setColor(Color.CYAN);
                     int bottomY = 300;
-                    g.fillRect(100, bottomY - 100, 40, 100);
-                    g.fillRect(140, bottomY - 200, 40, 200);
-                    g.fillRect(180, bottomY - 150, 40, 150);
-                    g.fillRect(220, bottomY - 125, 40, 125);
-                    g.fillRect(260, bottomY - 170, 40, 170);
-                    g.setColor(Color.RED);
-                    g.setFont(font);
-                    g.drawString(data[0], 130, 250);
-                    g.drawString(data[1], 130, 270);
+                    graphics.fillRect(100, bottomY - 100, 40, 100);
+                    graphics.fillRect(140, bottomY - 200, 40, 200);
+                    graphics.fillRect(180, bottomY - 150, 40, 150);
+                    graphics.fillRect(220, bottomY - 125, 40, 125);
+                    graphics.fillRect(260, bottomY - 170, 40, 170);
+                    graphics.setColor(Color.RED);
+                    graphics.setFont(font);
+                    graphics.drawString(barChartData[0], 130, 250);
+                    graphics.drawString(barChartData[1], 130, 270);
                 }
             } else {
                 int bottomY = 500;
-                g.setColor(Color.CYAN);
-                g.fillRect(112, bottomY - 200, 75, 200);
-                g.fillRect(187, bottomY - 400, 75, 400);
-                g.fillRect(262, bottomY - 300, 75, 300);
-                g.fillRect(337, bottomY - 250, 75, 250);
-                g.fillRect(412, bottomY - 340, 75, 340);
+                graphics.setColor(Color.CYAN);
+                graphics.fillRect(112, bottomY - 200, 75, 200);
+                graphics.fillRect(187, bottomY - 400, 75, 400);
+                graphics.fillRect(262, bottomY - 300, 75, 300);
+                graphics.fillRect(337, bottomY - 250, 75, 250);
+                graphics.fillRect(412, bottomY - 340, 75, 340);
                 font = new Font("Arial Black", Font.BOLD, 55);
-                g.setColor(Color.BLACK);
-                g.setFont(font);
-                g.drawString(data[0], 130, 400);
+                graphics.setColor(Color.BLACK);
+                graphics.setFont(font);
+                graphics.drawString(barChartData[0], 130, 400);
             }
         } else {
             if (chartMode.equals("rpfll")) {
                 font = new Font("Bookman Old Style", Font.BOLD, 55);
-                g.setColor(Color.WHITE);
-                g.setFont(font);
-                g.drawString(specialData.get(0), 200, 340);
+                graphics.setColor(Color.WHITE);
+                graphics.setFont(font);
+                graphics.drawString(largePieChartData.get(0), 200, 340);
             } else {
                 font = new Font("Bookman Old Style", Font.BOLD, 30);
-                g.setFont(font);
-                g.setColor(Color.WHITE);
-                g.drawString(smallPieChartData[0], 145, 205);
-                g.drawString(smallPieChartData[1], 170, 235);
+                graphics.setFont(font);
+                graphics.setColor(Color.WHITE);
+                graphics.drawString(smallPieChartData[0], 145, 205);
+                graphics.drawString(smallPieChartData[1], 170, 235);
             }
         }
-        if ((data != null && (data.length ^ 0x54) == 50) || (specialData.contains("Monthly"))
+        if ((barChartData != null && (barChartData.length ^ 0x54) == 50) || (largePieChartData.contains("Monthly"))
                 || getTitle().contains("daily")) {
             try {
                 repaint(200);
