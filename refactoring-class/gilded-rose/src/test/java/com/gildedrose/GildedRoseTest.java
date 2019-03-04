52e165fd54cb053193fe6e3e9cb3a36cd8f1c0c1
xing <xing@integral.io>
2019-03-04 14:36:10 -0500

    created golden master

--- refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,7 +7,9 @@ public class GildedRoseTest {
 
     @Test
     public void characterize() throws Exception {
-        CombinationApprovals.verifyAllCombinations(this::processInput, new String[]{"af"}, new Integer[]{2}, new Integer[]{4});
+        CombinationApprovals.verifyAllCombinations(this::processInput, new String[]{"Aged Brie", "Sulfuras, Hand of Ragnaros", "", "hello My name is Lana and I am a cute puppy dog",
+                        "Backstage passes to a TAFKAL80ETC concert"},
+                new Integer[]{2, 4, 0, -1, 10, 55, 7}, new Integer[]{4, 2, 0, -3000, 10, 55});
     }
 
     private Item processInput(String name, int sellln, int quality) {
