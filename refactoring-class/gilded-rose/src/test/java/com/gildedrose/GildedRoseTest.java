862120f964e338d798616530644954f83a72d479
xing <xing@integral.io>
2019-03-04 14:08:16 -0500

    implemented processInput method

--- refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,15 +1,19 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
     public void characterize() throws Exception {
-        final Item item = new Item("af", 2, 4);
+        CombinationApprovals.verifyAllCombinations(this::processInput, new String[]{"af"}, new Integer[]{2}, new Integer[]{4});
+    }
+
+    private Item processInput(String name, int sellln, int quality) {
+        final Item item = new Item(name, sellln, quality);
         final GildedRose gildedRose = new GildedRose(new Item[]{item});
         gildedRose.updateQuality();
-        Approvals.verify(item);
+        return item;
     }
 }
