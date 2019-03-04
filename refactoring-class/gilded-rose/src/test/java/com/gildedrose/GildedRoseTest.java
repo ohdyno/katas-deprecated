d3349f3cdb2cf94e83de18dd1fc5b7db538d1810
xing <xing@integral.io>
2019-03-04 13:46:03 -0500

    added toString in Item class

--- refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,11 +1,13 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
     public void characterize() throws Exception {
-
+        final Item item = new Item("af", 2, 4);
+        Approvals.verify(item);
     }
 }
