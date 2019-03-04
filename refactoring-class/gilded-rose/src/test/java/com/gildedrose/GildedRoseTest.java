227a14734d746f41dd23d688130e43d9c7830665
xing <xing@integral.io>
2019-03-04 13:50:08 -0500

    test updateQuality()

--- refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ refactoring-class/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,6 +8,8 @@ public class GildedRoseTest {
     @Test
     public void characterize() throws Exception {
         final Item item = new Item("af", 2, 4);
+        final GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
         Approvals.verify(item);
     }
 }
