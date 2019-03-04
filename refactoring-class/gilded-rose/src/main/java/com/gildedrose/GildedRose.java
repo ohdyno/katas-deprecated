d73f043b0335842aab1a3068b50cc11414ea140d
xing <xing@integral.io>
2019-03-04 15:11:43 -0500

    created handleBackstagePasses

--- refactoring-class/gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ refactoring-class/gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -16,8 +16,37 @@ class GildedRose {
                 handleAgedBrie(item);
                 continue;
             }
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                handleBackstagePasses(item);
+                continue;
+            }
             foo(item);
         }
+    }
+
+    private void handleBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+        return;
     }
 
     private void handleAgedBrie(Item item) {
