1b8c5e79df3d30f08be9c5b502d22d21bd542fb7
xing <xing@integral.io>
2019-09-10 10:53:15 -0400

    remove clutter

--- refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
+++ refactoring-class/src/main/java/org/chartsmart/IndvDsp.java
@@ -12,18 +12,6 @@ public class IndvDsp extends JPanel {
         private String str_n2;
         private double value;
 
-        public double convertTo(Unit unit) {
-            return 0;
-        }
-
-        public void setName(String name) {
-            this.str_n2 = name;
-        }
-
-        public String getName() {
-            return str_n2;
-        }
-
         public void setValue(double v1) {
             this.value = v1;
         }
@@ -35,10 +23,8 @@ public class IndvDsp extends JPanel {
 
     private String jjD;
     private String __APARAM__Z;
-    private String[] horizontalLabelNames;
-    private String[] verticalLabelNames;
     private int ct;
-    private String chartTitle;
+
 
     /**
      * InitializeDrawArea
