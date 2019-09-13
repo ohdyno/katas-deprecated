b2fed9bf52c825666cb4a6c87befd2d49d019920
xing <xing@integral.io>
2019-09-13 16:59:17 -0400

    rendered single and shared barchart methods

--- refactoring-class/src/main/java/org/chartsmart/ChartPanel.java
+++ refactoring-class/src/main/java/org/chartsmart/ChartPanel.java
@@ -55,8 +55,6 @@ public class ChartPanel extends JPanel {
     }
 
     private void drawChart(Graphics graphics) {
-
-        // Render chart background
         renderChartBackground(graphics);
         renderChartForeground(graphics);
     }
@@ -105,29 +103,37 @@ public class ChartPanel extends JPanel {
 
     private void setBarChartGraphics(Graphics graphics, String[] barTitle) {
         if (chartMode.equals(SHARED_DISPLAY)) {
-            graphics.setColor(Color.CYAN);
-            int bottomY = 300;
-            graphics.fillRect(100, bottomY - 100, 40, 100);
-            graphics.fillRect(140, bottomY - 200, 40, 200);
-            graphics.fillRect(180, bottomY - 150, 40, 150);
-            graphics.fillRect(220, bottomY - 125, 40, 125);
-            graphics.fillRect(260, bottomY - 170, 40, 170);
-            graphics.setColor(Color.RED);
-            graphics.setFont(new Font("Arial Black", Font.BOLD, 25));
-            graphics.drawString(barTitle[0], 130, 250);
-            graphics.drawString(barTitle[1], 130, 270);
+            renderSharedBarChart(graphics, barTitle);
         } else {
-            int bottomY = 500;
-            graphics.setColor(Color.CYAN);
-            graphics.fillRect(112, bottomY - 200, 75, 200);
-            graphics.fillRect(187, bottomY - 400, 75, 400);
-            graphics.fillRect(262, bottomY - 300, 75, 300);
-            graphics.fillRect(337, bottomY - 250, 75, 250);
-            graphics.fillRect(412, bottomY - 340, 75, 340);
-            graphics.setColor(Color.BLACK);
-            graphics.setFont(new Font("Arial Black", Font.BOLD, 55));
-            graphics.drawString(barTitle[0], 130, 400);
+            renderSingleBarChart(graphics, barTitle[0]);
         }
+    }
+
+    private void renderSingleBarChart(Graphics graphics, String str) {
+        int bottomY = 500;
+        graphics.setColor(Color.CYAN);
+        graphics.fillRect(112, bottomY - 200, 75, 200);
+        graphics.fillRect(187, bottomY - 400, 75, 400);
+        graphics.fillRect(262, bottomY - 300, 75, 300);
+        graphics.fillRect(337, bottomY - 250, 75, 250);
+        graphics.fillRect(412, bottomY - 340, 75, 340);
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(new Font("Arial Black", Font.BOLD, 55));
+        graphics.drawString(str, 130, 400);
+    }
+
+    private void renderSharedBarChart(Graphics graphics, String[] barTitle) {
+        graphics.setColor(Color.CYAN);
+        int bottomY = 300;
+        graphics.fillRect(100, bottomY - 100, 40, 100);
+        graphics.fillRect(140, bottomY - 200, 40, 200);
+        graphics.fillRect(180, bottomY - 150, 40, 150);
+        graphics.fillRect(220, bottomY - 125, 40, 125);
+        graphics.fillRect(260, bottomY - 170, 40, 170);
+        graphics.setColor(Color.RED);
+        graphics.setFont(new Font("Arial Black", Font.BOLD, 25));
+        graphics.drawString(barTitle[0], 130, 250);
+        graphics.drawString(barTitle[1], 130, 270);
     }
 
     private void renderChartBackground(Graphics graphics) {
