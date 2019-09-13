e3ffa5b2ad4d9076e90d3322b9cf734b4af923c8
xing <xing@integral.io>
2019-09-13 17:17:54 -0400

    reorganized code

--- refactoring-class/src/main/java/org/chartsmart/ChartPanel.java
+++ refactoring-class/src/main/java/org/chartsmart/ChartPanel.java
@@ -27,22 +27,6 @@ public class ChartPanel extends JPanel {
         }
     }
 
-    private void setPieChartTitle() {
-        if (chartMode.equals(SINGLE_MODE)) {
-            title = "Pie Chart - Single Mode";
-        } else {
-            title = "Pie Chart - Compare Mode";
-        }
-    }
-
-    private void setBarChartTitle() {
-        if (chartMode.equals(SINGLE_MODE)) {
-            title = "Bar Chart - Single Mode";
-        } else {
-            title = "Bar Chart - Compare Mode";
-        }
-    }
-
     String getTitle() {
         return title;
     }
@@ -72,6 +56,86 @@ public class ChartPanel extends JPanel {
         }
     }
 
+    private void renderChartBackground(Graphics graphics) {
+        if (chartType == BAR_CHART) {
+            renderBarChartBackground(graphics);
+        } else {
+            renderPieChartBackground(graphics);
+        }
+    }
+
+    private void setBarChartTitle() {
+        if (chartMode.equals(SINGLE_MODE)) {
+            title = "Bar Chart - Single Mode";
+        } else {
+            title = "Bar Chart - Compare Mode";
+        }
+    }
+
+    private void renderBarChartBackground(Graphics graphics) {
+        if (chartMode.equals(SINGLE_MODE)) {
+            graphics.setColor(Color.RED);
+            graphics.fillRect(100, 90, getWidth() - 200, 420);
+        } else {
+            graphics.setColor(Color.BLACK);
+            graphics.fillRect(95, 95, 210, 210);
+        }
+    }
+
+    private void renderBarChart(Graphics graphics) {
+        String[] barTitle;
+        if (chartMode.equals(SINGLE_MODE)) {
+            barTitle = new String[1];
+            barTitle[0] = "Bar Chart";
+        } else {
+            barTitle = new String[2];
+            barTitle[0] = "Bar Chart";
+            barTitle[1] = "Small";
+        }
+        if (chartMode.equals(SHARED_DISPLAY)) {
+            graphics.setColor(Color.CYAN);
+            int bottomY = 300;
+            graphics.fillRect(100, bottomY - 100, 40, 100);
+            graphics.fillRect(140, bottomY - 200, 40, 200);
+            graphics.fillRect(180, bottomY - 150, 40, 150);
+            graphics.fillRect(220, bottomY - 125, 40, 125);
+            graphics.fillRect(260, bottomY - 170, 40, 170);
+            graphics.setColor(Color.RED);
+            graphics.setFont(new Font("Arial Black", Font.BOLD, 25));
+            graphics.drawString(barTitle[0], 130, 250);
+            graphics.drawString(barTitle[1], 130, 270);
+        } else {
+            int bottomY = 500;
+            graphics.setColor(Color.CYAN);
+            graphics.fillRect(112, bottomY - 200, 75, 200);
+            graphics.fillRect(187, bottomY - 400, 75, 400);
+            graphics.fillRect(262, bottomY - 300, 75, 300);
+            graphics.fillRect(337, bottomY - 250, 75, 250);
+            graphics.fillRect(412, bottomY - 340, 75, 340);
+            graphics.setColor(Color.BLACK);
+            graphics.setFont(new Font("Arial Black", Font.BOLD, 55));
+            graphics.drawString(barTitle[0], 130, 400);
+        }
+    }
+
+    private void setPieChartTitle() {
+        if (chartMode.equals(SINGLE_MODE)) {
+            title = "Pie Chart - Single Mode";
+        } else {
+            title = "Pie Chart - Compare Mode";
+        }
+    }
+
+    private void renderPieChartBackground(Graphics graphics) {
+        if (chartMode.equals(SINGLE_MODE)) {
+            graphics.setColor(Color.BLUE);
+            graphics.fillOval(100, 100, 450, getHeight() - 150);
+        } else {
+            graphics.setColor(Color.BLUE);
+            graphics.fillOval(100, 100, 225, 225);
+        }
+    }
+
     private void renderPieChart(Graphics graphics) {
         String[] pieTitle = new String[0];
         List<String> pieTitle2 = new ArrayList<>();
@@ -93,78 +157,4 @@ public class ChartPanel extends JPanel {
             graphics.drawString(pieTitle[1], 170, 235);
         }
     }
-
-    private void renderBarChart(Graphics graphics) {
-        String[] barTitle;
-        if (chartMode.equals(SINGLE_MODE)) {
-            barTitle = new String[1];
-            barTitle[0] = "Bar Chart";
-        } else {
-            barTitle = new String[2];
-            barTitle[0] = "Bar Chart";
-            barTitle[1] = "Small";
-        }
-        if (chartMode.equals(SHARED_DISPLAY)) {
-            renderSharedBarChart(graphics, barTitle);
-        } else {
-            renderSingleBarChart(graphics, barTitle[0]);
-        }
-    }
-
-    private void renderSingleBarChart(Graphics graphics, String str) {
-        int bottomY = 500;
-        graphics.setColor(Color.CYAN);
-        graphics.fillRect(112, bottomY - 200, 75, 200);
-        graphics.fillRect(187, bottomY - 400, 75, 400);
-        graphics.fillRect(262, bottomY - 300, 75, 300);
-        graphics.fillRect(337, bottomY - 250, 75, 250);
-        graphics.fillRect(412, bottomY - 340, 75, 340);
-        graphics.setColor(Color.BLACK);
-        graphics.setFont(new Font("Arial Black", Font.BOLD, 55));
-        graphics.drawString(str, 130, 400);
-    }
-
-    private void renderSharedBarChart(Graphics graphics, String[] barTitle) {
-        graphics.setColor(Color.CYAN);
-        int bottomY = 300;
-        graphics.fillRect(100, bottomY - 100, 40, 100);
-        graphics.fillRect(140, bottomY - 200, 40, 200);
-        graphics.fillRect(180, bottomY - 150, 40, 150);
-        graphics.fillRect(220, bottomY - 125, 40, 125);
-        graphics.fillRect(260, bottomY - 170, 40, 170);
-        graphics.setColor(Color.RED);
-        graphics.setFont(new Font("Arial Black", Font.BOLD, 25));
-        graphics.drawString(barTitle[0], 130, 250);
-        graphics.drawString(barTitle[1], 130, 270);
-    }
-
-    private void renderChartBackground(Graphics graphics) {
-        if (chartType == BAR_CHART) {
-            renderBarChartBackground(graphics);
-        } else {
-            renderPieChartBackground(graphics);
-        }
-    }
-
-    private void renderPieChartBackground(Graphics graphics) {
-        if (chartMode.equals(SINGLE_MODE)) {
-            graphics.setColor(Color.BLUE);
-            graphics.fillOval(100, 100, 450, getHeight() - 150);
-        } else {
-            graphics.setColor(Color.BLUE);
-            graphics.fillOval(100, 100, 225, 225);
-        }
-    }
-
-    private void renderBarChartBackground(Graphics graphics) {
-        if (chartMode.equals(SINGLE_MODE)) {
-            graphics.setColor(Color.RED);
-            graphics.fillRect(100, 90, getWidth() - 200, 420);
-        } else {
-            graphics.setColor(Color.BLACK);
-            graphics.fillRect(95, 95, 210, 210);
-        }
-    }
-
-
 }
